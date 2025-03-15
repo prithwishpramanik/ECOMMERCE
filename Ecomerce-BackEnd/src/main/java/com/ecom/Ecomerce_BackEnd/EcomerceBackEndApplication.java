59c79a7148e33da1b3b9fd30e520e98dff72bf45
Prithwish Pramanik <prithwishpramanik462@gmail.com>
2025-03-15 23:23:03 +0530

    initial Backend Setup done

---
+++ Ecomerce-BackEnd/src/main/java/com/ecom/Ecomerce_BackEnd/EcomerceBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.Ecomerce_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceBackEndApplication.class, args);
+	}
+
+}
---
+++ Ecomerce-BackEnd/src/test/java/com/ecom/Ecomerce_BackEnd/EcomerceBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.Ecomerce_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
