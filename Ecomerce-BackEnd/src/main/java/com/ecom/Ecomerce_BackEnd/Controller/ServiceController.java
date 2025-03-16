4e5bc22c02e02d4b6a2db50e73d16ac3e085f060
Prithwish Pramanik <prithwishpramanik462@gmail.com>
2025-03-16 19:23:36 +0530

    ResponseEntity added to set customised http Status REport

--- Ecomerce-BackEnd/src/main/java/com/ecom/Ecomerce_BackEnd/Controller/ServiceController.java
+++ Ecomerce-BackEnd/src/main/java/com/ecom/Ecomerce_BackEnd/Controller/ServiceController.java
@@ -3,6 +3,8 @@ package com.ecom.Ecomerce_BackEnd.Controller;
 import com.ecom.Ecomerce_BackEnd.Model.Product;
 import com.ecom.Ecomerce_BackEnd.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,8 @@ public class ServiceController {
     private ProductService ProductService;
 
     @GetMapping("/products")
-    public List<Product> getProducts(){
-        return ProductService.getProducts();
+    public ResponseEntity<List<Product>> getProducts(){
+
+        return new ResponseEntity<>(ProductService.getProducts(), HttpStatus.OK) ;
     }
 }
